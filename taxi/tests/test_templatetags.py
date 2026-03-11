5458c13e1a7ce8f67daffea60733da7c723e9251
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-03-11 15:42:10 +0200

    fixed flake8 mistakes

--- taxi/tests/test_templatetags.py
+++ taxi/tests/test_templatetags.py
@@ -15,16 +15,16 @@ class QueryTransformTagTests(TestCase):
         return {k: v if len(v) > 1 else v[0] for k, v in parsed.items()}
 
     def test_updates_existing_param(self):
-        req = self.factory.get('/?a=1&b=2')
-        result = query_transform(req, a='42')
-        assert self._qs_to_dict(result) == {'a': '42', 'b': '2'}
+        req = self.factory.get("/?a=1&b=2")
+        result = query_transform(req, a="42")
+        assert self._qs_to_dict(result) == {"a": "42", "b": "2"}
 
     def test_removes_param_when_value_is_none(self):
-        req = self.factory.get('/?a=1&b=2')
+        req = self.factory.get("/?a=1&b=2")
         result = query_transform(req, b=None)
-        assert self._qs_to_dict(result) == {'a': '1'}
+        assert self._qs_to_dict(result) == {"a": "1"}
 
     def test_adds_new_param(self):
-        req = self.factory.get('/?a=1')
-        result = query_transform(req, page='3')
-        assert self._qs_to_dict(result) == {'a': '1', 'page': '3'}
+        req = self.factory.get("/?a=1")
+        result = query_transform(req, page="3")
+        assert self._qs_to_dict(result) == {"a": "1", "page": "3"}
